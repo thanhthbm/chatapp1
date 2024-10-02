8b48c7d4f579b675fc8efd5011ffa8602ff4498c
Vũ Minh Thành <52306715+thanhthbm@users.noreply.github.com>
2024-10-02 09:56:40 +0700

    Update Main.java
    a

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Scanner sc = new Scanner(System.in);
+        sc.nextLine();
     }
 }
